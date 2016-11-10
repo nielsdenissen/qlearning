7ec693b79f2044ce95174b7cacd4b7d8642b66be
Niels Denissen <nielsdenissen@MacNiels.local>
2016-11-10 23:26:44 +0100

    Fix adjusting values in qmatrix

--- qlearning.py
+++ qlearning.py
@@ -22,7 +22,7 @@ class Qlearning:
         :param discount_factor: the discount factor used for Q-learning
         """
         # Initialize the matrix with Q-values
-        init_data = [[initial_reward for _ in possible_states]
+        init_data = [[float(initial_reward) for _ in possible_states]
                      for _ in possible_actions]
         self._qmatrix = pd.DataFrame(data=init_data,
                                      index=possible_actions,
@@ -53,11 +53,12 @@ class Qlearning:
         """
         # Update q_value for a state-action pair Q(s,a):
         # Q(s,a) = Q(s,a) + α( r + γmaxa' Q(s',a') - Q(s,a) )
-        q_sa = self._qmatrix[state][action]
-        max_q_sa_next = self._qmatrix[next_state][self.get_best_action(next_state)]
+        q_sa = self._qmatrix.ix[action, state]
+        max_q_sa_next = self._qmatrix.ix[self.get_best_action(next_state), next_state]
         r = reward
         alpha = self._learn_rate
         gamma = self._discount_factor
 
         # Do the computation
-        self._qmatrix[state][action] = q_sa + alpha * (r + gamma * max_q_sa_next - q_sa)
+        new_q_sa = q_sa + alpha * (r + gamma * max_q_sa_next - q_sa)
+        self._qmatrix.set_value(action, state, new_q_sa)
